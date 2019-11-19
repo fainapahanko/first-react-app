f26e292db070b824a7428727fbf3b75b7e51d8af
Faina Poganko <fainapoganko@MacBook-Air-Faina.local>
2019-11-19 13:03:11 +0100

    first commit

---
+++ src/Components/MainComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MainConponent extends React.Component {
+    state = {}
+    render() {
+        return(
+            <h1>Lol</h1>
+        )
+    }
+}
+
+export default MainConponent
